6667e5faaea1da38b0bad52bf40e7aeb8b81a079
Joe Johnson <johnson.joseph527@gmail.com>
2019-02-06 12:44:56 -0700

    Organized JS and prepend game cards

--- script.js
+++ script.js
@@ -30,21 +30,29 @@ var count = 0;
 
 // Event Listeners
 
+cardArea.addEventListener('click', function(event) {
+  if (event.target.className === 'close-card') {
+    event.target.parentNode.parentNode.parentNode.remove()
+  }
+});
+
 clearButton.addEventListener("click", function(e) {
   e.preventDefault();
   clear();
 });
 
-updateButton.addEventListener("click", function(e) {
+for (var i = 0; i < inputs.length; i++) {
+  inputs[i].addEventListener('keyup', disableButtons);
+}
+
+resetButton.addEventListener("click", function(e) {
   e.preventDefault();
-  minRange = parseInt(minRangeQuery.value);
-  maxRange = parseInt(maxRangeQuery.value);
-  if(!checkEmpty(0,1) && validateRange(minRange, maxRange)) {
-    minRangeSmall.innerText = minRange;
-    maxRangeSmall.innerText = maxRange;
-    random = generateRandomNumber(minRange, maxRange);
-    console.log(random);
+  for(var i = 0; i < inputs.length; i++) {
+    inputs[i].value = "";
   }
+  // minRangeQuery.value = 1;
+  // maxRangeQuery.value = 100;
+  reset();
 });
 
 submitButton.addEventListener('click', function(e) {
@@ -60,34 +68,68 @@ submitButton.addEventListener('click', function(e) {
   }
 });
 
-for (var i = 0; i < inputs.length; i++) {
-  inputs[i].addEventListener('keyup', disableButtons);
-}
-
-function updateResults(guess1, guess2) {
-  names[0].innerText = challengerOneName.value;
-  names[1].innerText = challengerTwoName.value;
-  scores[0].innerText = guess1;
-  scores[1].innerText = guess2;
-  checkGuess(text[0], guess1);
-  checkGuess(text[1], guess2);
-}
-
-resetButton.addEventListener("click", function(e) {
+updateButton.addEventListener("click", function(e) {
   e.preventDefault();
-  for(var i = 0; i < inputs.length; i++) {
-    inputs[i].value = "";
+  minRange = parseInt(minRangeQuery.value);
+  maxRange = parseInt(maxRangeQuery.value);
+  if(!checkEmpty(0,1) && validateRange(minRange, maxRange)) {
+    minRangeSmall.innerText = minRange;
+    maxRangeSmall.innerText = maxRange;
+    random = generateRandomNumber(minRange, maxRange);
+    console.log(random);
   }
-  // minRangeQuery.value = 1;
-  // maxRangeQuery.value = 100;
-  reset();
 });
 
-cardArea.addEventListener('click', function(event) {
-  if (event.target.className === 'close-card') {
-    event.target.parentNode.parentNode.parentNode.remove()
+
+function addCard(winner, count) {
+  var clone = cardTemplate.content.cloneNode(true);
+  clone.getElementById("game-card-c1").innerText = challengerOneName.value;
+  clone.getElementById("game-card-c2").innerText = challengerTwoName.value;
+  clone.querySelector(".winner").innerText = winner;
+  clone.querySelector(".count").innerText = count;
+  cardArea.insertBefore(clone, cardArea.firstChild);
+}
+
+function addDraw(count) {
+  var clone = drawTemplate.content.cloneNode(true);
+  clone.getElementById("game-card-c1").innerText = challengerOneName.value;
+  clone.getElementById("game-card-c2").innerText = challengerTwoName.value;
+  clone.querySelector(".winner").innerText = "Draw";
+  clone.querySelector(".count").innerText = count;
+  cardArea.insertBefore(clone, cardArea.firstChild);
+}
+
+function changeRange(e) {
+  minRange -= 10;
+  maxRange += 10;
+  minRangeQuery.placeholder = minRange;
+  maxRangeQuery.placeholder = maxRange;
+}
+
+function checkEmpty(start, end) {
+  var empty = false;
+  for(var i = start; i < end + 1; i++) {
+    if(inputs[i].value == "") {
+      inputs[i].classList.add("empty");
+      errors[i].classList.remove("hidden");
+      empty = true;
+    } else {
+      inputs[i].classList.remove("empty");
+      errors[i].classList.add("hidden");
+    }
   }
-});
+  return empty;
+}
+
+function checkGuess(text, guess) {
+  if(guess < random) {
+    text.innerText = "too low";
+  } else if (guess > random) {
+    text.innerText = "too high";
+  } else {
+    text.innerText = "BOOM!";
+  }
+}
 
 function clear() {
   for(var i = 2; i < inputs.length; i++) {
@@ -96,41 +138,35 @@ function clear() {
   // disableButtons();
 }
 
-function reset() {
-  cardArea.innerHTML = "";
-  minRange = 1;
-  maxRange = 100;
-  minRangeQuery.placeholder = 1;
-  maxRangeQuery.placeholder = 100;
-  newGame();
-  resetResults();
-  disableButtons();
+function disableButtons(e) {
+  disableReset();
+  disableClear();
 }
 
-function newGame() {
-  for(i = 2; i < inputs.length; i++) {
-    inputs[i].value = "";
-  }
-  minRangeQuery.value = "";
-  maxRangeQuery.value = "";
-  minRangeSmall.innerText = minRange;
-  maxRangeSmall.innerText = maxRange;
-  random = generateRandomNumber(minRange, maxRange);
-  disableButtons();
+function disableClear(e) {
+  if (challengerOneName.value !== ''
+      || challengerOneGuessQuery.value !== ''
+      || challengerTwoName.value !== ''
+      || challengerTwoGuessQuery.value !== '')
+    {
+      clearButton.disabled = false;
+    } else {
+      clearButton.disabled = true;
+    }
 }
 
-function resetResults() {
-  for(var i = 0; i < 2; i++) {
-    names[i].innerText = "-";
-    scores[i].innerText = "--";
-    text[i].innerText = "-";
-  }
-}
-
-function validateGuess(num, element) {
-  if(num < minRange || num > maxRange) {
-    element.value = "";
-  }
+function disableReset(e) {
+  if (minRangeQuery.value !== ''
+      || maxRangeQuery.value !== ''
+      || challengerOneName.value !== ''
+      || challengerOneGuessQuery.value !== ''
+      || challengerTwoName.value !== ''
+      || challengerTwoGuessQuery.value !== '')
+    {
+      resetButton.disabled = false;
+    } else {
+      resetButton.disabled = true;
+    }
 }
 
 function generateRandomNumber(min, max) {
@@ -156,32 +192,56 @@ function getWinner(guess1, guess2, count) {
   }
 }
 
-function checkGuess(text, guess) {
-  if(guess < random) {
-    text.innerText = "too low";
-  } else if (guess > random) {
-    text.innerText = "too high";
-  } else {
-    text.innerText = "BOOM!";
+function newGame() {
+  for(i = 2; i < inputs.length; i++) {
+    inputs[i].value = "";
+  }
+  minRangeQuery.value = "";
+  maxRangeQuery.value = "";
+  minRangeSmall.innerText = minRange;
+  maxRangeSmall.innerText = maxRange;
+  random = generateRandomNumber(minRange, maxRange);
+  disableButtons();
+}
+
+function reset() {
+  cardArea.innerHTML = "";
+  minRange = 1;
+  maxRange = 100;
+  minRangeQuery.placeholder = 1;
+  maxRangeQuery.placeholder = 100;
+  newGame();
+  resetResults();
+  disableButtons();
+}
+
+function resetResults() {
+  for(var i = 0; i < 2; i++) {
+    names[i].innerText = "-";
+    scores[i].innerText = "--";
+    text[i].innerText = "-";
   }
 }
 
-function addDraw(count) {
-  var clone = drawTemplate.content.cloneNode(true);
-  clone.getElementById("game-card-c1").innerText = challengerOneName.value;
-  clone.getElementById("game-card-c2").innerText = challengerTwoName.value;
-  clone.querySelector(".winner").innerText = "Draw";
-  clone.querySelector(".count").innerText = count;
-  cardArea.appendChild(clone);
+function startGame() {
+  minRange = 1;
+  maxRange = 100;
+  newGame();
 }
 
-function addCard(winner, count) {
-  var clone = cardTemplate.content.cloneNode(true);
-  clone.getElementById("game-card-c1").innerText = challengerOneName.value;
-  clone.getElementById("game-card-c2").innerText = challengerTwoName.value;
-  clone.querySelector(".winner").innerText = winner;
-  clone.querySelector(".count").innerText = count;
-  cardArea.appendChild(clone);
+function updateResults(guess1, guess2) {
+  names[0].innerText = challengerOneName.value;
+  names[1].innerText = challengerTwoName.value;
+  scores[0].innerText = guess1;
+  scores[1].innerText = guess2;
+  checkGuess(text[0], guess1);
+  checkGuess(text[1], guess2);
+}
+
+function validateGuess(num, element) {
+  if(num < minRange || num > maxRange) {
+    element.value = "";
+  }
 }
 
 function validateRange(min, max) {
@@ -196,65 +256,6 @@ function validateRange(min, max) {
     valid = true;
   }
   return valid;
-}
-
-function checkEmpty(start, end) {
-  var empty = false;
-  for(var i = start; i < end + 1; i++) {
-    if(inputs[i].value == "") {
-      inputs[i].classList.add("empty");
-      errors[i].classList.remove("hidden");
-      empty = true;
-    } else {
-      inputs[i].classList.remove("empty");
-      errors[i].classList.add("hidden");
-    }
-  }
-  return empty;
-}
-
-function startGame() {
-  minRange = 1;
-  maxRange = 100;
-  newGame();
-}
-
-function disableButtons(e) {
-  disableReset();
-  disableClear();
-}
-
-function disableReset(e) {
-  if (minRangeQuery.value !== ''
-      || maxRangeQuery.value !== ''
-      || challengerOneName.value !== ''
-      || challengerOneGuessQuery.value !== ''
-      || challengerTwoName.value !== ''
-      || challengerTwoGuessQuery.value !== '')
-    {
-      resetButton.disabled = false;
-    } else {
-      resetButton.disabled = true;
-    }
-}
-
-function disableClear(e) {
-  if (challengerOneName.value !== ''
-      || challengerOneGuessQuery.value !== ''
-      || challengerTwoName.value !== ''
-      || challengerTwoGuessQuery.value !== '')
-    {
-      clearButton.disabled = false;
-    } else {
-      clearButton.disabled = true;
-    }
-}
-
-function changeRange(e) {
-  minRange -= 10;
-  maxRange += 10;
-  minRangeQuery.placeholder = minRange;
-  maxRangeQuery.placeholder = maxRange;
 }
 
 startGame();
